0767e5a7d647951fbd1e4b45312c4637444aef5b
jorgemartincacereslopez <jorgemartincacereslopez@gmail.com>
2022-02-16 12:09:46 -0300

    Comentarios agregados y descripcion de la extension actualizada.

--- extension.js
+++ extension.js
@@ -1,30 +1,37 @@
+// Especificacion de las librerias requeridas
 const vscode = require('vscode');
-
 const axios = require('axios');
 
-
+// Definicion del parametro context
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+// Activacion de la extension
 function activate(context) {
 
-	console.log('Congratulations, your extension "penguin-troubleshooting" is now active!');
+	// Mensaje de inicio en la linea de consola
+	console.log('Iniciando Pengu Help Me.');
 	
+	// Registro del comando 'penguin-troubleshooting.penguserver' como disposable
 	let disposable = vscode.commands.registerCommand('penguin-troubleshooting.penguserver', async function () {
+		
+		// Ventana de input para que el usuario ingrese el error
 		let input_value = await vscode.window.showInputBox({
 			prompt:"Pengu, help me please.",
 			placeHolder: 'Syntax error...',
 		});
 
+		// Definicion del servidor donde se aloja la base de datos
 		const url = "https://pengu-server.herokuapp.com/procesar"
-	
+
+		// La extension manda un request al server con el error del usuario
 		const respuesta = await axios.put(url, {validacion: input_value})
 
+		// La respuesta del server se visualiza en una notificacion
 		vscode.window.showInformationMessage(respuesta.data)
 
-
 	});
-
 
 	context.subscriptions.push(disposable);
 }
